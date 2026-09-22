a5b620551eccc9a3ac6bb4c859ce04b12ece01e5
niraj <nirajbhayera4@gmail.com>
2026-09-22 14:08:20 +0530

    Add Greet component and use it in App
    Add a new Greet component at hello/src/components/Greet.js (ES6 arrow function) that renders "Hello Niraj". Update hello/src/App.js to import Greet and replace the default header/logo/link content with <Greet />.

---
+++ hello/src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/*
+// this is a normal function without es6
+function Greet(){
+    return <h1>Hello Niraj</h1>
+}
+    */
+// this is with es6 syntax
+
+const Greet=()=>{
+    return <h1>Hello Niraj</h1>
+}
+export default Greet
