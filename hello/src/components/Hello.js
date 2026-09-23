6a5bda063716377faa4dc17177761ac95d11bf76
niraj <nirajbhayera4@gmail.com>
2026-09-23 19:22:14 +0530

    update hello.js
    This commit shows the implementation of code with and without JSX .

--- hello/src/components/Hello.js
+++ hello/src/components/Hello.js
@@ -1,9 +1,15 @@
 import React from 'react';
 const Hello=()=>{
+    /*
+    // this is with JSX syntax
     return (
-        <div>
+        <div className="dummyclass">
             <h1>Hello React</h1>
         </div>
     )
+    */
+
+    // this is without JSX syntax
+   return React.createElement('div',{id:'hello', className:'dummyclass'},React.createElement('h1',null,'Hello React'))
 }
 export default Hello 
