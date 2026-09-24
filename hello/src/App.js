e6a0dcd8218e0ba9cc1552dc0a270fd8696311ed
niraj <nirajbhayera4@gmail.com>
2026-09-24 16:13:34 +0530

    Add Counter component and render in App
    Introduce a Counter class component at hello/src/components/Counter.js that manages a count state. Uses functional setState in increment and provides incrementFive which calls increment five times. Renders current count and an Increment button. Import and render <Counter /> in hello/src/App.js and comment out the previously rendered Message component.

--- hello/src/App.js
+++ hello/src/App.js
@@ -4,16 +4,18 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
+      <Counter />
       {/* <Greet /> */}
       {/*<Greet name="bruce" hero="superman"><p>This is children props</p></Greet>
       <Greet name="james" hero="batman"/>
       <Greet name="peter" hero="spiderman"/>
       */}
-      <Message />
+      {/*<Message />*/}
 
            {/* <Welcome /> */}
       {/* <Hello /> */}
