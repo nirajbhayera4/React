5d5893bb8e48cc742bced9f3645c919862242735
niraj <nirajbhayera4@gmail.com>
2026-09-26 19:57:14 +0530

    Refactor event handler binding
    Updated EventBind to use a class field arrow handler instead of binding in the constructor, aligning the example with the recommended React event binding pattern. The older binding approaches remain commented for reference.

--- hello/src/components/EventBind.js
+++ hello/src/components/EventBind.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react'
 
  class EventBind extends Component {
+    //3. approach---> binding inside constructor method
     constructor(props) {
       super(props)
     
       this.state = {
         message:"hello"
       }
-      this.clickhandler=this.clickhandler.bind(this)
+      //this.clickhandler=this.clickhandler.bind(this)
     }
-    clickhandler(){
+    /*clickhandler(){
         this.setState({
             message:'Goodbye!'
         })
        console.log(this)
     }
+       */
+
+    //this is 4th appraoch 
+      clickhandler= ()=>{
+        this.setState({
+            message:'Goodbye!'
+        })
+      }
   render() {
     return (
       <div>
         <div>{this.state.message}</div>
+
+        {/*1. approach -->binding inside render*/}
         {/*<button onClick={this.clickhandler.bind(this)}>
             Click me please
         </button>*/}
